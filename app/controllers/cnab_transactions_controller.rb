e290bb2b495adcf8bdf9b36615bbb646a923c4e0
Daniel <danielcandidosilwa@gmail.com>
2023-08-15 22:57:37 -0300

    Adjusting controllers to respond in API format

--- app/controllers/cnab_transactions_controller.rb
+++ app/controllers/cnab_transactions_controller.rb
@@ -2,6 +2,11 @@ class CnabTransactionsController < ApplicationController
 
   def index
     @cnab_transactions = CnabTransaction.paginate(page: params[:page], per_page: 10)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @cnab_transactions }
+    end
   end
 
   def new
@@ -12,13 +17,20 @@ class CnabTransactionsController < ApplicationController
     file_path = cnab_transaction_params[:file].tempfile.path
     parse_cnab_transaction = ParseCnabTransaction.new(file_path)
     if parse_cnab_transaction.execute
-      flash[:notice] = "Arquivo importado com sucesso"
-      redirect_to action: :index
+      respond_to do |format|
+        format.html { redirect_to action: :index, notice: "Arquivo importado com sucesso" }
+        format.json { render json: { message: "Arquivo importado com sucesso" }, status: :created }
+      end
     else
-      flash.now[:alert] = "Erro ao importar arquivo: #{parse_cnab_transaction.errors.join(', ')}"
-      render :new
+      respond_to do |format|
+        format.html do
+          flash.now[:alert] = "Erro ao importar arquivo: #{parse_cnab_transaction.errors.join(', ')}"
+          render :new
+        end
+        format.json { render json: { error: "Erro ao importar arquivo: #{parse_cnab_transaction.errors.join(', ')}" }, status: :unprocessable_entity }
+      end
     end
-  end  
+  end
 
   private
 
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,10 @@
 class StoresController < ApplicationController
   def index
     @stores = Store.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @stores }
+    end
   end
 end
